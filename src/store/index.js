af46adb5225536708c29f77e95e418f89961fc9e
unsnruu <unsnruu@gmail.com>
2022-09-17 03:08:01 +0900

    [#7] feat : commentReducer 추가 및 sagaMiddleware 실행
    commentReducer를 configureStore의 옵션 중 reducer 객체의 property로 설정하였습니다.
    또한 sagaMiddleware를 run 메서드를 호출하였습니다.

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,17 @@ import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 
+import commentReducer from '../redux/comment/slice';
+
 const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    comment: commentReducer,
+  },
   devTools: process.env.NODE_ENV !== 'production',
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger, sagaMiddleware),
 });
+
+//todo : rootSaga 추가하기
+sagaMiddleware.run();
