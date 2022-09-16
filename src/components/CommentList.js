b89a76ab53a21e240fddbd1fa3c951e6f80797f5
unsnruu <unsnruu@gmail.com>
2022-09-17 03:14:02 +0900

    [#7] refactor : styled-components의 위치 변경
    styled-components의 위치를 상단에서 하단으로 변경하였습니다.
    그 외에 data 상수를 제거하였습니다.

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
 
+function CommentList({ comments }) {
+  return comments.map((comment, key) => (
+    <Comment key={key}>
+      <img src={comment.profile_url} alt="" />
+
+      {comment.author}
+
+      <CreatedAt>{comment.createdAt}</CreatedAt>
+
+      <Content>{comment.content}</Content>
+
+      <Button>
+        <a>수정</a>
+        <a>삭제</a>
+      </Button>
+
+      <hr />
+    </Comment>
+  ));
+}
+export default CommentList;
+
 const Comment = styled.div`
   padding: 7px 10px;
   text-align: left;
@@ -34,37 +56,3 @@ const Button = styled.div`
     cursor: pointer;
   }
 `;
-
-// 임시 데이터 입니다. 코드 작성시 data 부분을 지워주세요
-const data = [
-  {
-    id: 1,
-    profile_url: 'https://picsum.photos/id/1/50/50',
-    author: 'abc_1',
-    content: 'UI 테스트는 어떻게 진행하나요',
-    createdAt: '2020-05-01',
-  },
-];
-
-function CommentList() {
-  return data.map((comment, key) => (
-    <Comment key={key}>
-      <img src={comment.profile_url} alt="" />
-
-      {comment.author}
-
-      <CreatedAt>{comment.createdAt}</CreatedAt>
-
-      <Content>{comment.content}</Content>
-
-      <Button>
-        <a>수정</a>
-        <a>삭제</a>
-      </Button>
-
-      <hr />
-    </Comment>
-  ));
-}
-
-export default CommentList;
