b773d1445d5e36e4be5c901212facb26f0b1b6cc
unsnruu <unsnruu@gmail.com>
2022-09-17 02:12:33 +0900

    [#7] feat : getComments 구현
    getComments를 fetch를 통해 기본적인 구현을 마쳤습니다.

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,21 @@
-export default class Api {
+class Api {
   constructor() {
     this.baseUrl = 'http://localhost:4000/comments';
   }
-  async getComments() {}
+  async getComments() {
+    try {
+      const res = await fetch(this.baseUrl);
+      const comments = await res.json();
+      return comments;
+    } catch (err) {
+      throw new Error('Failed to get comments from server');
+    }
+  }
   async getAComment() {}
   async createAComment() {}
   async deleteAComment() {}
   async updateAComment() {}
 }
+
+const api = new Api();
+export default api;
