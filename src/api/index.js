8d2dd758aeb6d34f6b6131d60ba6599d541b83fc
unsnruu <unsnruu@gmail.com>
2022-09-17 01:56:55 +0900

    [#7] feat : api를 위한 클래스 정의
    api와 관련된 사항을 한 곳에서 관리하기 위해서 class 형태로 선언하여 감쌌습니다.

---
+++ src/api/index.js
@@ -0,0 +1,10 @@
+export default class Api {
+  constructor() {
+    this.baseUrl = 'http://localhost:4000/comments';
+  }
+  async getComments() {}
+  async getAComment() {}
+  async createAComment() {}
+  async deleteAComment() {}
+  async updateAComment() {}
+}
