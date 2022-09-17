14d062eb9949ba47dad6997eefe8813172e3ffd5
unsnruu <unsnruu@gmail.com>
2022-09-17 22:22:37 +0900

    [#7] feat : modeReducer rootReducer에 추가
    modeReducer를 rootReducer에 추가하였습니다.

--- src/redux/index.js
+++ src/redux/index.js
@@ -3,6 +3,7 @@ import { all } from 'redux-saga/effects';
 import commentReducer from './comment/slice';
 import paginationReducer from './pagination/slice';
 import formReducer from './form/slice';
+import modeReducer from './mode/slice';
 
 import { watchComments } from './comment/saga';
 import { watchPagination } from './pagination/saga';
@@ -12,6 +13,7 @@ export const rootReducer = {
   comment: commentReducer,
   pagination: paginationReducer,
   form: formReducer,
+  mode: modeReducer,
 };
 
 export function* rootSaga() {
