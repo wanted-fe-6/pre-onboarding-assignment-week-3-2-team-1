3085001304b3c1021a1657c653f98cf72837ed43
unsnruu <unsnruu@gmail.com>
2022-09-17 16:39:11 +0900

    [#7] fix : 데이터를 제대로 받아오지 못하는 문제 해결
    화면에 데이터를 제대로 받아오지 못하는 문제를 해결하였습니다.
    slice의 reducer에서 comments property가
    data로 이름이 바뀌면서 생기는 문제를 해결하였습니다.

--- src/containers/CommentListContainer.js
+++ src/containers/CommentListContainer.js
@@ -5,10 +5,11 @@ import CommentList from '../components/CommentList';
 import { getComments } from '../redux/comment/slice';
 
 function CommentListContainer() {
-  const comments = useSelector(state => state.comment.comments);
+  const comments = useSelector(state => state.comment.data);
   const dispatch = useDispatch();
 
   useEffect(() => {
+    //todo : page를 위해서 redux를 새롭게 짜야할 것 같기도 한데?
     dispatch(getComments({ page: 1 }));
   }, []);
 
