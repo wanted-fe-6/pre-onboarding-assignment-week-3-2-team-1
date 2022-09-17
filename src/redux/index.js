27a1b1e867787943042be6bcbd9eb260fff7f3ec
unsnruu <unsnruu@gmail.com>
2022-09-17 20:42:16 +0900

    [#7] feat : form saga와 reducer를 rootReducer와 rootSaga에 등록
    form saga와 form reducer를 rootReducer와 rootReducer에 등록하였습니다.

--- src/redux/index.js
+++ src/redux/index.js
@@ -2,14 +2,18 @@ import { all } from 'redux-saga/effects';
 
 import commentReducer from './comment/slice';
 import paginationReducer from './pagination/slice';
+import formReducer from './form/slice';
+
 import { watchComments } from './comment/saga';
 import { watchPagination } from './pagination/saga';
+import { watchForm } from './form/saga';
 
 export const rootReducer = {
   comment: commentReducer,
   pagination: paginationReducer,
+  form: formReducer,
 };
 
 export function* rootSaga() {
-  yield all([watchComments(), watchPagination()]);
+  yield all([watchComments(), watchPagination(), watchForm()]);
 }
