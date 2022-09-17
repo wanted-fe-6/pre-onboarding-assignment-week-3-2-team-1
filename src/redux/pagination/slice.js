e672e9c2e273a2fad9bf380e072e9206578151aa
unsnruu <unsnruu@gmail.com>
2022-09-17 18:22:18 +0900

    [#7] fix : getTotalPages 액션 수행시 데이터가 꼬이는 문제 해결
    reducerUtils.loading에 전달하는 인자의 값이
    state로 전달되는 바람에 자기자신을 가리키는 값이 data에 들어갔습니다.
    오류를 수정하여 새롭게 생성한 state.data값을 전달하였습니다.

--- src/redux/pagination/slice.js
+++ src/redux/pagination/slice.js
@@ -1,21 +1,21 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { reducerUtils } from '../../util/async.utill';
 
-//? 데이터의 구조를 어떻게 만들어야 할까?
-
 const initialState = reducerUtils.initial({ current: 1, total: 1 });
 const paginationSlice = createSlice({
   name: 'pagination',
   initialState,
   reducers: {
-    getTotalPages: state => reducerUtils.loading(state),
+    getTotalPages: state => reducerUtils.loading({ ...state.data }),
     getTotalPagesSuccess: (state, { payload }) =>
       reducerUtils.success({ ...state.data, total: payload }),
     getTotalPagesError: (_, { payload }) => reducerUtils.error(payload),
+    //todo: 수정해야함.
     incrementPage: state => state + 1,
     decrementPage: state => state - 1,
   },
 });
+
 export const {
   decrementPage,
   incrementPage,
