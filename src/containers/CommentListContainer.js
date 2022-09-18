f0c773ed3788ea50f87cc48c365f7e88c8eb351f
gomgun <zxolqs@gmail.com>
2022-09-18 23:40:17 +0900

    [#6] feat : CommentListContainer 추가
    CommentList 컴포넌트의 Logic을 처리함

--- src/containers/CommentListContainer.js
+++ src/containers/CommentListContainer.js
@@ -1,8 +1,62 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 import CommentList from '../components/CommentList';
 
-function CommentListContainer() {
-  return <CommentList />;
+import { deleteComment as deleteCommentApi } from '../api/comments';
+import { PAGE_PER_COMMENTS } from '../constant';
+import {
+  initComments,
+  changeTotalPages,
+  changeCurPage,
+  changeCurPageComments,
+  deleteComment,
+} from '../store/localComments';
+import {
+  changeProfileUrl,
+  changeAuthor,
+  changeContent,
+  changeCreatedAt,
+  setFormId,
+} from '../store/form';
+
+function CommentListContainer({ comments }) {
+  const { curPageComments } = useSelector(state => state.localComments);
+  const dispatch = useDispatch();
+
+  const onDeleteComments = id => {
+    if (confirm('삭제하시겠습니까?')) {
+      deleteCommentApi(id);
+      dispatch(deleteComment(id));
+      // refactor : page 변화 -> curPageComments 계산 가능
+      dispatch(changeCurPage(1));
+      dispatch(changeCurPageComments(1));
+    }
+  };
+
+  const setFormComments = comment => {
+    dispatch(setFormId(comment.id));
+    // refactor : form state 단일 관리
+    dispatch(changeProfileUrl(comment.profile_url));
+    dispatch(changeAuthor(comment.author));
+    dispatch(changeContent(comment.content));
+    dispatch(changeCreatedAt(comment.createdAt));
+  };
+
+  useEffect(() => {
+    // refactor : init시 comments의 개수로 totalPage 계산 가능
+    dispatch(initComments(comments.data));
+    dispatch(changeTotalPages(Math.ceil(comments.data.length / PAGE_PER_COMMENTS)));
+    dispatch(changeCurPageComments());
+  }, []);
+
+  return (
+    <CommentList
+      curPageComments={curPageComments}
+      onDeleteComments={onDeleteComments}
+      setFormComments={setFormComments}
+    />
+  );
 }
 
 export default CommentListContainer;
