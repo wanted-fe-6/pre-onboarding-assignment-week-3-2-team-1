a87aa963125aa4f3e69bf6e1c33e8107a15f6617
gomgun <zxolqs@gmail.com>
2022-09-18 23:39:00 +0900

    [#6] feat : Form 컴포넌트 추가

--- src/components/Form.js
+++ src/components/Form.js
@@ -24,22 +24,43 @@ const FormStyle = styled.div`
   }
 `;
 
-function Form() {
+function Form({ onSubmit, formContent, dispatch, dispatchFunctions }) {
   return (
     <FormStyle>
-      <form>
+      <form onSubmit={e => onSubmit(e)}>
         <input
           type="text"
           name="profile_url"
           placeholder="https://picsum.photos/id/1/50/50"
+          value={formContent.profile_url}
+          onChange={e => dispatch(dispatchFunctions.changeProfileUrl(e.target.value))}
           required
         />
         <br />
-        <input type="text" name="author" placeholder="작성자" />
+        <input
+          type="text"
+          name="author"
+          placeholder="작성자"
+          value={formContent.author}
+          onChange={e => dispatch(dispatchFunctions.changeAuthor(e.target.value))}
+        />
         <br />
-        <textarea name="content" placeholder="내용" required></textarea>
+        <textarea
+          name="content"
+          placeholder="내용"
+          value={formContent.content}
+          onChange={e => dispatch(dispatchFunctions.changeContent(e.target.value))}
+          required
+        ></textarea>
         <br />
-        <input type="text" name="createdAt" placeholder="2020-05-30" required />
+        <input
+          type="text"
+          name="createdAt"
+          placeholder="2020-05-30"
+          value={formContent.createdAt}
+          onChange={e => dispatch(dispatchFunctions.changeCreatedAt(e.target.value))}
+          required
+        />
         <br />
         <button type="submit">등록</button>
       </form>
