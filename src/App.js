45fd0c1df07a065769dec94b80aa5010f0e3c31c
gomgun <zxolqs@gmail.com>
2022-09-18 23:25:45 +0900

    [#6] feat : App 컴포넌트 추가
    App 컴포넌트 mount 시 server data fetch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
 import CommentListContainer from './containers/CommentListContainer';
 import PageListContainer from './containers/PageListContainer';
 import FormContainer from './containers/FormContainer';
 
+import { getAllComments } from './store/comments';
+
 function App() {
+  const { data, loading, error } = useSelector(state => state.comments.comments);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (data) return;
+    dispatch(getAllComments());
+  }, []);
+
+  if (loading) return <div>로딩중...</div>;
+  if (error) return <div>에러 발생!</div>;
+  if (!data) return null;
   return (
     <div>
-      <CommentListContainer />
+      <CommentListContainer comments={data} />
       <PageListContainer />
       <FormContainer />
     </div>
