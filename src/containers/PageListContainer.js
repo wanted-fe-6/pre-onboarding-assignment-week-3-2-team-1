a583a6a735d6157c22f07585b64a2bd1d43abb84
gomgun <zxolqs@gmail.com>
2022-09-18 23:41:34 +0900

    [#6] feat : PageListContainer 추가
    PageList 컴포넌트의 logic을 담당함
    - pagination
    - changeCurPage
    - changeCurPageComments

--- src/containers/PageListContainer.js
+++ src/containers/PageListContainer.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 import PageList from '../components/PageList';
 
+import { changeCurPage, changeCurPageComments } from '../store/localComments';
+
 function PageListContainer() {
-  return <PageList />;
+  const { totalpages, curPage } = useSelector(state => state.localComments);
+  const dispatch = useDispatch();
+
+  const pageArray = Array.from({ length: totalpages }, (_, i) => i + 1);
+
+  const onChangePage = page => {
+    dispatch(changeCurPage(page));
+    dispatch(changeCurPageComments(page));
+  };
+
+  return <PageList pageArray={pageArray} onChangePage={onChangePage} curPage={curPage} />;
 }
 
 export default PageListContainer;
