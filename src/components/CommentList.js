65a4ba60d26a4d87909ea4e533f326bf7ae6a03f
kunnie-code <kearnyyoon@gmail.com>
2022-09-18 12:04:43 +0900

    [#1] feat : 댓글 리스트 가져오기 구현

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,5 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
+import { getComments } from '../modules/comments';
+
+function CommentList() {
+  const { data, loading, error } = useSelector(state => state.comments.commentList);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getComments(1));
+  }, [dispatch]);
+
+  if (loading) return <div>로딩중...</div>;
+  if (error) return <div>에러가 발생했습니다.</div>;
+  if (!data) return null;
+
+  return data.contents.map((comment, key) => (
+    <Comment key={key}>
+      <img src={comment.profile_url} alt="" />
+
+      {comment.author}
+
+      <CreatedAt>{comment.createdAt}</CreatedAt>
+
+      <Content>{comment.content}</Content>
+
+      <Button>
+        <a>수정</a>
+        <a>삭제</a>
+      </Button>
+
+      <hr />
+    </Comment>
+  ));
+}
+
+export default CommentList;
 
 const Comment = styled.div`
   padding: 7px 10px;
@@ -34,37 +70,3 @@ const Button = styled.div`
     cursor: pointer;
   }
 `;
-
-// 임시 데이터 입니다. 코드 작성시 data 부분을 지워주세요
-const data = [
-  {
-    id: 1,
-    profile_url: 'https://picsum.photos/id/1/50/50',
-    author: 'abc_1',
-    content: 'UI 테스트는 어떻게 진행하나요',
-    createdAt: '2020-05-01',
-  },
-];
-
-function CommentList() {
-  return data.map((comment, key) => (
-    <Comment key={key}>
-      <img src={comment.profile_url} alt="" />
-
-      {comment.author}
-
-      <CreatedAt>{comment.createdAt}</CreatedAt>
-
-      <Content>{comment.content}</Content>
-
-      <Button>
-        <a>수정</a>
-        <a>삭제</a>
-      </Button>
-
-      <hr />
-    </Comment>
-  ));
-}
-
-export default CommentList;
